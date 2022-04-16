3ca354e41a6fb167d6f78f3a7909532b35f302ac
Phan John <thaibinh120702@gmail.com>
2022-04-16 14:48:19 +0700

    complete ide

--- compiler/tmp/newNASM.asm
+++ compiler/tmp/newNASM.asm
@@ -3,8 +3,6 @@ SECTION .data
 fmt: db "%s = %lld", 10, 0
 Label_id1 : dq 0
 Label_idOrigin1 : dq "X0"
-Label_id2 : dq 0
-Label_idOrigin2 : dq "Y"
 SECTION .text
 global main 
  main: 
@@ -12,20 +10,9 @@ global main
 mov rcx, [Label_id1] 
  add rcx, 1 
  mov [Label_id1], rcx 
-mov rcx, [Label_id1] 
- add rcx, 1 
- mov [Label_id1], rcx 
-mov rcx, [Label_id2] 
- add rcx, 1 
- mov [Label_id2], rcx 
 mov rdi, fmt 
  mov rsi, Label_idOrigin1 
  mov rdx, [Label_id1] 
- mov rax, 0 
- call printf 
-mov rdi, fmt 
- mov rsi, Label_idOrigin2 
- mov rdx, [Label_id2] 
  mov rax, 0 
  call printf 
 pop rbp 
